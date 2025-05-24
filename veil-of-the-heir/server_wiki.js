1fd56bdc3d583a9465e70fa2fdd6a3776712daf9
Chathushka <chathushkahome9@gmail.com>
2025-05-25 01:41:19 +0530

    change

---
+++ veil-of-the-heir/server_wiki.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5001;
+
+// Serve the entire project directory as static
+app.use(express.static(path.join(__dirname)));
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
